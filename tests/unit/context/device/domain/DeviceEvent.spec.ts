54d202008df519d8ceff666954be9c295c97be46
Massimo Biagioli <massimo.biagioli@ideato.info>
2022-04-07 18:03:09 +0200

    Add device domain test

--- tests/unit/context/device/domain/DeviceEvent.spec.ts
+++ tests/unit/context/device/domain/DeviceEvent.spec.ts
@@ -1,5 +1,10 @@
 import * as E from 'fp-ts/lib/Either'
 import * as DeviceEvent from '../../../../../src/context/device/domain/DeviceEvent'
+import { DeviceEventType } from '../../../../../src/context/device/domain/DeviceEvent'
+import {
+  DeviceConnectedEvent,
+  DeviceDisconnectedEvent
+} from '../../../../../src/context/device/domain/DeviceEventEmitter'
 
 describe('DeviceEvent', () => {
   it('should create a DeviceEvent from raw string', () => {
@@ -48,5 +53,45 @@ describe('DeviceEvent', () => {
       const error = result.left
       expect(error.message).toBe('malformed input data')
     }
+  })
+
+  it('should create a dispatchable DeviceConnectedEvent', () => {
+    const deviceEvent = {
+      eventType: DeviceEventType.connected,
+      sender: {
+        id: '123',
+        model: 'ESP8266',
+        address: '10.10.10.10',
+        sketch: 'blink'
+      },
+      createdAt: 1588888888
+    }
+
+    const dispatchableEvent = DeviceEvent.toDispatchableEvent(deviceEvent)
+    const dispatchableConnectedEvent = dispatchableEvent as DeviceConnectedEvent
+
+    expect(dispatchableConnectedEvent.id).toBe('123')
+    expect(dispatchableConnectedEvent.model).toBe('ESP8266')
+    expect(dispatchableConnectedEvent.address).toBe('10.10.10.10')
+    expect(dispatchableConnectedEvent.sketch).toBe('blink')
+  })
+
+  it('should create a dispatchable DeviceDisconnectedEvent', () => {
+    const deviceEvent = {
+      eventType: DeviceEventType.disconnected,
+      sender: {
+        id: '123',
+        model: 'ESP8266',
+        address: '10.10.10.10',
+        sketch: 'blink'
+      },
+      createdAt: 1588888888
+    }
+
+    const dispatchableEvent = DeviceEvent.toDispatchableEvent(deviceEvent)
+    const dispatchableConnectedEvent =
+      dispatchableEvent as DeviceDisconnectedEvent
+
+    expect(dispatchableConnectedEvent.id).toBe('123')
   })
 })
